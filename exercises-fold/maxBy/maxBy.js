ac4e9f73d0d9fce61afe89fd6721a8003b2ae00c
wismith <56857148+wismith@users.noreply.github.com>
2020-01-23 20:54:49 -0800

    maxBy exercise w/ fold

--- exercises-fold/maxBy/maxBy.js
+++ exercises-fold/maxBy/maxBy.js
@@ -16,8 +16,34 @@
  * @param {function} fn - A function that returns a `number`
  * @returns {any} The element `x` in `collection` with the largest value of `fn(x)`.
  */
-function maxBy(collection, predicate) {
-  // This is your job. :)
+function maxBy(collection, fn) {
+  return fold(collection,function(acc,item){
+    if (!acc){
+      acc = collection[0];
+    }
+    if (fn(item) > fn(acc)){
+      acc = item;
+    }
+    return acc;
+  }, undefined);
+}
+
+function strLength(str){
+  return str.length;
+}
+
+function square(n){
+  return n*n;
+}
+
+
+
+function fold(collection, fn, init){
+  let acc = init;
+  for (let item of collection){
+    acc = fn(acc, item);
+  }
+  return acc;
 }
 
 if (require.main === module) {
@@ -26,6 +52,9 @@ if (require.main === module) {
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
   // How can you be sure it's returning the FIRST thing it finds? Does it matter?
+
+  console.log(maxBy(['hello', 'friend', 'how', 'are', 'you'],strLength) === 'friend');
+  console.log(maxBy([1,5,3,4,2],square) === 5);
 }
 
 module.exports = maxBy;
